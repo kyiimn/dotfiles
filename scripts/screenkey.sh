2290cc0e644ed3779cd5a9b05f06a69591bc9bca
Kim Yin <kyiimn@gmail.com>
2021-04-28 21:50:44 +0900

    Update screenkey.sh

--- scripts/screenkey.sh
+++ scripts/screenkey.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 if [ "$1" = 'run' ]; then
-	ps ax | grep screenkey | grep python | grep -v grep || screenkey &
+	ps ax | grep screenkey | grep python | grep -v grep || screenkey --scr 1 &
 elif [ "$1" = 'kill' ]; then
 	pkill -9 screenkey
 elif [ "$1" = 'toggle' ]; then
 	ST=`ps ax | grep screenkey | grep python | grep -v grep | wc -l`
 	if [ $ST -eq 0 ]; then
-		ps ax | grep screenkey | grep python | grep -v grep || screenkey &
+		ps ax | grep screenkey | grep python | grep -v grep || screenkey --scr 1 &
 	else
 		pkill -9 screenkey
 	fi
